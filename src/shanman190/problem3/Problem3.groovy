298fc3e24b259390a5859f74b461015e72bee59f
Shannon Pamperl <spamperl@shelterinsurance.com>
2013-12-17 09:45:22 -0600

    Added problem to Problem 3.

--- src/shanman190/problem3/Problem3.groovy
+++ src/shanman190/problem3/Problem3.groovy
@@ -1,7 +1,8 @@
 package shanman190.problem3
 
-600851475143
-
+// The prime factors of 13195 are 5, 7, 13 and 29.
+//
+// What is the largest prime factor of the number 600851475143 ?
 def testPrime = 600851475143
 def divisor = 2
 
