371cb44333eef3f96e85e1458a002663b14a2115
Shannon <shanman190@gmail.com>
2014-07-21 21:59:38 -0500

    Solution to problem 1 using java 8.

---
+++ src/shanman190/problem1/Problem1.java
@@ -0,0 +1,14 @@
+package shanman190.problem1;
+
+import java.util.stream.IntStream;
+
+public class Problem1 {
+
+	public static void main(String[] args) {
+		System.out.println(
+				IntStream
+					.range(1, 1000)
+					.filter(i -> { return i % 3 == 0 || i % 5 == 0; })
+					.sum());
+	}
+}
