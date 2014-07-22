60ab1d4e99e82b5992ab72dd2f16afb65a946483
Shannon <shanman190@gmail.com>
2014-07-21 22:49:51 -0500

    Solution to problem 4 using java 8.

---
+++ src/shanman190/problem4/Problem4.java
@@ -0,0 +1,18 @@
+package shanman190.problem4;
+
+public class Problem4 {
+
+	public static void main(String[] args) {
+		String result = "";
+		for (int i = 999; i > 99; i--) {
+			for (int j = 999; j > 99; j--) {
+				String testPalindrome = String.valueOf(i * j);
+				if (testPalindrome.equals(new StringBuffer(testPalindrome).reverse().toString())
+						&& (result.equals("") || Long.parseLong(result) < Long.parseLong(testPalindrome))) {
+					result = testPalindrome;
+				}
+			}
+		}
+		System.out.println(result);
+	}
+}
