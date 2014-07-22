d20f4ab01554fa5facdfe2853e858ef39c980bcb
Shannon <shanman190@gmail.com>
2014-07-21 22:41:18 -0500

    Solution to problem 3 using java 8.

---
+++ src/shanman190/problem3/Problem3.java
@@ -0,0 +1,17 @@
+package shanman190.problem3;
+
+public class Problem3 {
+	
+	public static void main(String[] args) {
+		Long testPrime = 600851475143L;
+		Long divisor = 2L;
+		while (testPrime > 1) {
+			if (testPrime % divisor == 0)
+				testPrime = (testPrime / divisor);
+			else
+				divisor++;
+		}
+		System.out.println(divisor);
+	}
+
+}
